e7aeb02b3db1081709ee3f27807b6d0758dc074f
SAMPATH RAM M M <159137764+mmsampath777@users.noreply.github.com>
2025-04-15 15:24:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,29 @@
-from flask import Flask, render_template,redirect, session
+from flask import Flask, render_template, redirect, session
 from functools import wraps
 import pymongo
-import os
-
 
 app = Flask(__name__)
-app.secret_key=b'\xf3\xc0\xcd\x1c\x147\x96C\xecf\xdf\x02H\x1c\xa6\xa6'
+app.secret_key = b'\xf3\xc0\xcd\x1c\x147\x96C\xecf\xdf\x02H\x1c\xa6\xa6'
 
-
-
-CONNECTION_STRING = os.getenv("STRING")
+# MongoDB Setup
+CONNECTION_STRING = "mongodb+srv://sethupathyr:9PpYWkAQ8V16dHqf@cluster0.wjhrq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client = pymongo.MongoClient(CONNECTION_STRING)
 db = client.thirukkural_pazhagu
 
-#Decorators
+# Decorator
 def login_required(f):
     @wraps(f)
-    def wrap(*arg, **kwargs):
-        if'logged_in' in session:
-            return f(*arg, **kwargs)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
         else:
             return redirect('/')
     return wrap
 
-#Routes
-from user import routes
+# Import Routes
+from user.routes import *
 
+# Core Pages
 @app.route('/')
 def home():
     return render_template('login.html')
@@ -34,10 +32,9 @@ def home():
 def register():
     return render_template('register.html')
 
-
 @app.route('/index/')
 @login_required
-def index():    
+def index():
     return render_template('index.html')
 
 @app.route('/select_adhigaram')
@@ -45,13 +42,10 @@ def index():
 def select_adhigaram():
     return render_template('select_adhigaram.html')
 
-
 @app.route('/select_game')
 @login_required
 def select_game():
     return render_template('select_game.html')
 
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000)) 
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    app.run(debug=True)
